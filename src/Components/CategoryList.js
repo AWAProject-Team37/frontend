d6654499761260f7732c70fa3b66adc25f9705ed
vaisane <t0vaja01@students.oamk.fi>
2021-11-05 14:30:54 +0200

    searchfield functionality

--- src/Components/CategoryList.js
+++ src/Components/CategoryList.js
@@ -2,14 +2,17 @@ import React from 'react'
 import CategoryComponent from './CategoryComponent'
 const CategoryList = () => {
     let categories=[{
+        id: 1,
         category: "Hamburger",
         image: "https://i.imgur.com/pbxkmeZ.png",
         alt: "hamburger"
     },{
+        id: 2,
         category: "Kebab",
         image: "https://i.imgur.com/FID6fUA.png",
         alt: "kebab"
     },{
+        id: 3,
         category: "Pizza",
         image: "https://i.imgur.com/H4gsLm4.png",
         alt: "pizza"
@@ -18,7 +21,7 @@ const CategoryList = () => {
         <div>
             <h1>Category:</h1>
             <div className="categoryList">
-                {categories.map(item => <CategoryComponent {...item}/>)}
+                {categories.map(item => <CategoryComponent {...item} key={item.id}/>)}
             </div>
             
         </div>
--- src/Components/RestaurantList.js
+++ src/Components/RestaurantList.js
@@ -1,63 +1,12 @@
 import React from 'react'
 import RestaurantComponent from './RestaurantComponent'
-const RestaurantList = () => {
+const RestaurantList = (props) => {
 
-    let testData = [{
-        id: 1,
-        name: "McDonalds",
-        address: "street123",
-        operatingHours: "09-22",
-        category: "hamburger",
-        priceLevel: "€€",
-        deliveryTime: "20 min",
-        image: "https://i.imgur.com/ItjptGC.png",
-        alt: "mcdonalds"
-    },{
-        id: 2,
-        name: "Kebula",
-        address: "street123",
-        operatingHours: "09-22",
-        category: "kebab",
-        priceLevel: "€",
-        deliveryTime: "15 min",
-        image: "https://i.imgur.com/TLzAJIB.png",
-        alt: "kebabeläin"
-    },{
-        id: 3,
-        name: "Hesburger",
-        address: "street123",
-        operatingHours: "09-22",
-        category: "hamburger",
-        priceLevel: "€€",
-        deliveryTime: "20 min",
-        image: "https://i.imgur.com/ntb6u96.png",
-        alt: "hesburger"
-    },{
-        id: 4,
-        name: "Pizzeria",
-        address: "street123",
-        operatingHours: "14-22",
-        category: "pizza",
-        priceLevel: "€€€",
-        deliveryTime: "50 min",
-        image: "https://i.imgur.com/3QLMUkZ.png",
-        alt: "pizza"
-    },{
-        id: 4,
-        name: "Pizzeria",
-        address: "street123",
-        operatingHours: "14-22",
-        category: "pizza",
-        priceLevel: "€€€",
-        deliveryTime: "50 min",
-        image: "https://i.imgur.com/3QLMUkZ.png",
-        alt: "pizza"
-    }]
     return (
         <div>
             <h1>Restaurants:</h1>
             <div className="restaurantList">
-                {testData.map(item => <RestaurantComponent {...item} key={item.id}/>)}
+                {props.testData.map(item => <RestaurantComponent {...item} key={item.id}/>)}
             </div>
             
         </div>
