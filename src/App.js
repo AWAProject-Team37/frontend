9b88c39aeec65f9632f707da36040cc5530d1a70
vaisane <t0vaja01@students.oamk.fi>
2021-11-16 10:12:49 +0200

    frontpage structure done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import LoginPage from './Pages/LoginPage';
 import FrontPage from './Pages/FrontPage';
+import RestaurantPage from './Pages/RestaurantPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Route exact path="/" component={FrontPage}/>
       <Route path="/login" component={LoginPage}/>
+      <Route path="/restaurant/:id" component={RestaurantPage}/>
     </BrowserRouter>
   );
 }
--- src/Components/RestaurantComponent.js
+++ src/Components/RestaurantComponent.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import "../Styles/FrontPage.css"
+import { useHistory } from "react-router-dom";
 const RestaurantComponent = (props) => {
+
+    const history = useHistory();
+
+    const routeChange = () => {
+        let path = `restaurant/${props.id}`;
+        history.push(path);
+    }
+
+
     return (
-        <div className="restaurantComponent">
+        <div className="restaurantComponent" onClick={routeChange}>
             <img src={props.image} alt={props.alt} className="restaurantImg"></img>
             <div className="restaurantName">{props.name}</div>
             <div className="divider"></div>
