335847f0f4772f892aeac56feaae47d248eca645
vaisane <t0vaja01@students.oamk.fi>
2021-11-26 19:12:51 +0200

    login and register

--- src/Components/LoginView.js
+++ src/Components/LoginView.js
@@ -1,13 +1,33 @@
 import React from 'react'
+import axios from 'axios'
 import "../Styles/LoginPage.css"
 const LoginView = () => {
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try{
+            const result = await axios.post(`http://localhost:4000/login`, null ,{
+                auth: {
+                    username: event.target.email.value,
+                    password: event.target.password.value
+                }
+            });
+            const jwt = result.data.jwt;
+            console.log(jwt);
+        } catch(error) {
+            console.log(error);
+    
+     }
+    }
     return (
         <div className="loginForm">
-            <p>Email</p>
-            <input type="text" placeholder="Email..." className="loginFormInputField"></input>
-            <p>Password</p>
-            <input type="password" placeholder="Password..." className="loginFormInputField"></input>
-            <button className="loginFormButton">Login</button>
+            <form onSubmit={handleSubmit}>
+                <p>Email</p>
+                <input type="text" placeholder="Email..." className="loginFormInputField" name="email"></input>
+                <p>Password</p>
+                <input type="password" placeholder="Password..." className="loginFormInputField" name="password"></input>
+                <button className="loginFormButton" type="submit">Login</button>
+            </form>
         </div>
     )
 }
--- src/Components/RegisterView.js
+++ src/Components/RegisterView.js
@@ -1,20 +1,48 @@
+import axios from 'axios';
 import React from 'react'
 
 const RegisterView = () => {
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        let managerStatus;
+        if(event.target.AccountType.value === "Customer"){
+            managerStatus = false;
+        } else {
+            managerStatus = true;
+        }
+        try{
+            const result = await axios.post(`http://localhost:4000/register`, {
+                FirstName: event.target.FirstName.value,
+                LastName: event.target.LastName.value,
+                Email: event.target.Email.value,
+                Password: event.target.Password.value,
+                isManager: managerStatus
+            });
+            
+            console.log(result);
+        } catch(error){
+            console.log(error);
+        }
+        
+    }
     return (
         <div className="loginForm">
+            <form onSubmit={handleSubmit}>
+            <p>Firstname</p>
+            <input type="text" placeholder="Firstname..." className="loginFormInputField" name="FirstName"></input>
+            <p>Lastname</p>
+            <input type="text" placeholder="Lastname..." className="loginFormInputField" name="LastName"></input>
             <p>Email</p>
-            <input type="text" placeholder="Email..." className="loginFormInputField"></input>
+            <input type="text" placeholder="Email..." className="loginFormInputField" name="Email"></input>
             <p>Password</p>
-            <input type="password" placeholder="Password..." className="loginFormInputField"></input>
-            <p>Confirm password</p>
-            <input type="password" placeholder="Confirm password..." className="loginFormInputField"></input>
+            <input type="password" placeholder="Password..." className="loginFormInputField" name="Password"></input>
             <p>Account type</p>
-            <select placeholder="Choose account type" className="loginFormInputField">
+            <select placeholder="Choose account type" className="loginFormInputField" name="AccountType">
                 <option value="Customer">Customer</option>
-                <option value="Restaurant owner">Restaurant owner</option>
+                <option value="Restaurant manager">Restaurant manager</option>
             </select>
             <button className="loginFormButton">Register</button>
+            </form>
         </div>
     )
 }
